fb6ae7a78e81c3f64fc7757349e3ac8d477b1e9f
abhinavp06 <aparasharchd06@gmail.com>
2025-10-21 10:31:57 +0530

    basic http init

--- go/campaign-gateway/main.go
+++ go/campaign-gateway/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"abhinavp06/campaign-gateway/config"
 	"abhinavp06/campaign-gateway/util"
+
+	"net/http"
 )
 
 func main() {
@@ -10,7 +12,22 @@ func main() {
 	util.InitLogger()
 	logger := util.GetLogger()
 	config.LoadConfig()
+	cfg := config.GetConfig()
+
+	port := cfg.Port
+	if port == "" {
+		port = "3010"
+	}
+
+	if port[0] != ':' {
+		port = ":" + port
+	}
 
 	logger.Info("Starting campaign-gateway")
-	logger.Info("campaign-gateway is up and running!")
+	logger.Info("campaign-gateway is up and running on port", "port", port)
+	
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		logger.Error("Server failed to start", "error", err)
+	}
 }
