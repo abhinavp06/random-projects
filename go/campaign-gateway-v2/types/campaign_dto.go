02ebcf846ffa58ab30b0d62f5658b8f6da1ef725
abhinavp06 <aparasharchd06@gmail.com>
2025-10-23 22:46:44 +0530

    made filter optional for campaigns

--- go/campaign-gateway-v2/types/campaign_dto.go
+++ go/campaign-gateway-v2/types/campaign_dto.go
@@ -6,7 +6,7 @@ type Campaign struct {
 	Id		  string `json:"id"`
 	Name	  string `json:"name"`
 	Cron 	  string `json:"cron"`
-	Filter    string `json:"filter"`
+	Filter    *string `json:"filter"`
 	Enabled   bool   `json:"enabled"`
 	LastRun   *time.Time `json:"last_run"`
 	CreatedAt time.Time `json:"created_at"`
