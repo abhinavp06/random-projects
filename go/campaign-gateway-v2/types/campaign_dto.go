15b92490365cf67593bc21c2b0eadd7bdc6608af
abhinavp06 <aparasharchd06@gmail.com>
2025-10-23 22:42:53 +0530

    added campaign dto

--- go/campaign-gateway-v2/types/campaign_dto.go
+++ go/campaign-gateway-v2/types/campaign_dto.go
@@ -1,1 +1,14 @@
 package types
+
+import "time"
+
+type Campaign struct {
+	Id		  string `json:"id"`
+	Name	  string `json:"name"`
+	Cron 	  string `json:"cron"`
+	Filter    string `json:"filter"`
+	Enabled   bool   `json:"enabled"`
+	LastRun   *time.Time `json:"last_run"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
